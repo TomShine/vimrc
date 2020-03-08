33a2c298b23ab7926fd624c7ebba03f08ba8a930
TomShine <shangzongyu1111@gmail.com>
2020-03-08 19:09:05 +0800

    udpate install.sh

--- install.sh
+++ install.sh
@@ -1,78 +1,78 @@
 #!/usr/bin/env bash
 
 # Init option {{{
-Color_off='\033[0m'       # Text Reset
+Color_off='\033[0m' # Text Reset
 
 # terminal color template {{{
 # Regular Colors
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
+Black='\033[0;30m'  # Black
+Red='\033[0;31m'    # Red
+Green='\033[0;32m'  # Green
+Yellow='\033[0;33m' # Yellow
+Blue='\033[0;34m'   # Blue
+Purple='\033[0;35m' # Purple
+Cyan='\033[0;36m'   # Cyan
+White='\033[0;37m'  # White
 
 # Bold
-BBlack='\033[1;30m'       # Black
-BRed='\033[1;31m'         # Red
-BGreen='\033[1;32m'       # Green
-BYellow='\033[1;33m'      # Yellow
-BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
-BCyan='\033[1;36m'        # Cyan
-BWhite='\033[1;37m'       # White
+BBlack='\033[1;30m'  # Black
+BRed='\033[1;31m'    # Red
+BGreen='\033[1;32m'  # Green
+BYellow='\033[1;33m' # Yellow
+BBlue='\033[1;34m'   # Blue
+BPurple='\033[1;35m' # Purple
+BCyan='\033[1;36m'   # Cyan
+BWhite='\033[1;37m'  # White
 
 # Underline
-UBlack='\033[4;30m'       # Black
-URed='\033[4;31m'         # Red
-UGreen='\033[4;32m'       # Green
-UYellow='\033[4;33m'      # Yellow
-UBlue='\033[4;34m'        # Blue
-UPurple='\033[4;35m'      # Purple
-UCyan='\033[4;36m'        # Cyan
-UWhite='\033[4;37m'       # White
+UBlack='\033[4;30m'  # Black
+URed='\033[4;31m'    # Red
+UGreen='\033[4;32m'  # Green
+UYellow='\033[4;33m' # Yellow
+UBlue='\033[4;34m'   # Blue
+UPurple='\033[4;35m' # Purple
+UCyan='\033[4;36m'   # Cyan
+UWhite='\033[4;37m'  # White
 
 # Background
-On_Black='\033[40m'       # Black
-On_Red='\033[41m'         # Red
-On_Green='\033[42m'       # Green
-On_Yellow='\033[43m'      # Yellow
-On_Blue='\033[44m'        # Blue
-On_Purple='\033[45m'      # Purple
-On_Cyan='\033[46m'        # Cyan
-On_White='\033[47m'       # White
+On_Black='\033[40m'  # Black
+On_Red='\033[41m'    # Red
+On_Green='\033[42m'  # Green
+On_Yellow='\033[43m' # Yellow
+On_Blue='\033[44m'   # Blue
+On_Purple='\033[45m' # Purple
+On_Cyan='\033[46m'   # Cyan
+On_White='\033[47m'  # White
 
 # High Intensity
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
+IBlack='\033[0;90m'  # Black
+IRed='\033[0;91m'    # Red
+IGreen='\033[0;92m'  # Green
+IYellow='\033[0;93m' # Yellow
+IBlue='\033[0;94m'   # Blue
+IPurple='\033[0;95m' # Purple
+ICyan='\033[0;96m'   # Cyan
+IWhite='\033[0;97m'  # White
 
 # Bold High Intensity
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
+BIBlack='\033[1;90m'  # Black
+BIRed='\033[1;91m'    # Red
+BIGreen='\033[1;92m'  # Green
+BIYellow='\033[1;93m' # Yellow
+BIBlue='\033[1;94m'   # Blue
+BIPurple='\033[1;95m' # Purple
+BICyan='\033[1;96m'   # Cyan
+BIWhite='\033[1;97m'  # White
 
 # High Intensity backgrounds
-On_IBlack='\033[0;100m'   # Black
-On_IRed='\033[0;101m'     # Red
-On_IGreen='\033[0;102m'   # Green
-On_IYellow='\033[0;103m'  # Yellow
-On_IBlue='\033[0;104m'    # Blue
-On_IPurple='\033[0;105m'  # Purple
-On_ICyan='\033[0;106m'    # Cyan
-On_IWhite='\033[0;107m'   # White
+On_IBlack='\033[0;100m'  # Black
+On_IRed='\033[0;101m'    # Red
+On_IGreen='\033[0;102m'  # Green
+On_IYellow='\033[0;103m' # Yellow
+On_IBlue='\033[0;104m'   # Blue
+On_IPurple='\033[0;105m' # Purple
+On_ICyan='\033[0;106m'   # Cyan
+On_IWhite='\033[0;107m'  # White
 # }}}
 
 #S ystem name
@@ -80,9 +80,8 @@ System="$(uname -s)"
 
 # }}}
 
-
 # need_cmd {{{
-need_cmd () {
+need_cmd() {
     if ! hash "$1" &>/dev/null; then
         error "Need '$1' (command not found)"
         exit 1
@@ -108,19 +107,19 @@ error() {
     exit 1
 }
 
-warn () {
+warn() {
     msg "${Red}[✘]${Color_off} ${1}${2}"
 }
 # }}}
 
 # echo_with_color {{{
-echo_with_color () {
+echo_with_color() {
     printf '%b\n' "$1$2$Color_off" >&2
 }
 # }}}
 
 # install_vim {{{
-install_vim () {
+install_vim() {
     if [[ -f "$HOME/.vimrc" ]]; then
         mv "$HOME/.vimrc" "$HOME/.vimrc_back"
         success "Backup $HOME/.vimrc to $HOME/.vimrc_back"
@@ -128,12 +127,12 @@ install_vim () {
 
     if [[ -d "$HOME/.vim" ]]; then
         if [[ "$(readlink $HOME/.vim)" =~ \.ShangVim$ ]]; then
-        success "Installed ShangVim for vim"
+            success "Installed ShangVim for vim"
         else
-        mv "$HOME/.vim" "$HOME/.vim_back"
-        success "BackUp $HOME/.vim to $HOME/.vim_back"
-        ln -s "$HOME/.ShangVim" "$HOME/.vim"
-        success "Installed ShangVim for vim"
+            mv "$HOME/.vim" "$HOME/.vim_back"
+            success "BackUp $HOME/.vim to $HOME/.vim_back"
+            ln -s "$HOME/.ShangVim" "$HOME/.vim"
+            success "Installed ShangVim for vim"
         fi
     else
         ln -s "$HOME/.ShangVim" "$HOME/.vim"
@@ -143,12 +142,12 @@ install_vim () {
 # }}}
 
 # fetch_repo {{{
-fetch_repo () {
+fetch_repo() {
     if [[ -d "$HOME/.ShangVim" ]]; then
         info "Trying to update ShangVim"
         cd "$HOME/.ShangVim"
         git pull
-        cd - > /dev/null 2>&1
+        cd - >/dev/null 2>&1
         success "Successfully update ShangVim"
     else
         info "Trying to clone ShangVim"
@@ -159,7 +158,7 @@ fetch_repo () {
 # }}}
 
 # install_vim {{{
-install_vim () {
+install_vim() {
     if [[ -f "$HOME/.vimrc" ]]; then
         mv "$HOME/.vimrc" "$HOME/.vimrc_back"
         success "Backup $HOME/.vimrc to $HOME/.vimrc_back"
@@ -167,7 +166,7 @@ install_vim () {
         touch $HOME/.vimrc
     fi
 
-    echo "source $HOME/.ShangVim/init.vim" > "$HOME/.vimrc"
+    echo "source $HOME/.ShangVim/init.vim" >"$HOME/.vimrc"
 
     if [[ -d "$HOME/.vim" ]]; then
         if [[ "$(readlink $HOME/.vim)" =~ \.ShangVim$ ]]; then
@@ -184,7 +183,7 @@ install_vim () {
 # }}}
 
 # install_package_manager {{{
-install_package_manager () {
+install_package_manager() {
     if [[ ! -d "$HOME/.vim/autoload/plug.vim" ]]; then
         info "Install vim-plug start"
         curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
@@ -195,14 +194,14 @@ install_package_manager () {
 # }}}
 
 # install_package {{{
-install_package () {
+install_package() {
     vim +PlugInstall +qall
     success "vim install plugin sucess"
 }
 # }}}
 
 # uninstall_vim {{{
-uninstall_vim () {
+uninstall_vim() {
     if [[ -d "$HOME/.vim" ]]; then
         if [[ "$(readlink $HOME/.vim)" =~ \.ShangVim$ ]]; then
             rm "$HOME/.vim"
@@ -222,7 +221,7 @@ uninstall_vim () {
 # }}}
 
 # check_requirements {{{
-check_requirements () {
+check_requirements() {
     info "Checking Requirements for ShangVim"
     if hash "git" &>/dev/null; then
         git_version=$(git --version)
@@ -255,7 +254,7 @@ check_requirements () {
 # }}}
 
 # usage {{{
-usage () {
+usage() {
     echo "ShangVim install script : V ${Version}"
     echo ""
     echo "Usage : curl -sLf https://raw.githubusercontent.com/shangzongyu/vimrc/master/install.sh | bash -s -- [option] [target]"
@@ -286,7 +285,7 @@ usage () {
 # }}}
 
 # install_done {{{
-install_done () {
+install_done() {
     echo_with_color ${Yellow} ""
     echo_with_color ${Yellow} "Almost done!"
     echo_with_color ${Yellow} "=============================================================================="
@@ -298,7 +297,7 @@ install_done () {
 }
 # }}}
 
-welcome () {
+welcome() {
     echo_with_color ${Yellow} "        ___      ___ ___  _____ ______          "
     echo_with_color ${Yellow} "       |\  \    /  /|\  \|\   _ \  _   \        "
     echo_with_color ${Yellow} "       \ \  \  /  / | \  \ \  \\\__\ \  \       "
@@ -311,12 +310,13 @@ welcome () {
 # }}}
 
 # download_font {{{
-download_font () {
+download_font() {
     URL="https://raw.githubusercontent.com/wsdjeg/DotFiles/master/local/share/fonts/$1"
     PATH="$HOME/.local/share/fonts/$1"
     if [[ -f "$path" ]]; then
         success "Downloaded $1"
     else
+        info "URL=$URL"
         info "Downloading $1"
         curl -s -o "$PATH" "$URL"
         success "Downloaded $1"
@@ -325,7 +325,8 @@ download_font () {
 # }}}
 
 # install_fonts {{{
-install_fonts () {
+# 下载字体需要添加
+install_fonts() {
     if [[ ! -d "$HOME/.local/share/fonts" ]]; then
         mkdir -p $HOME/.local/share/fonts
     fi
@@ -344,50 +345,51 @@ install_fonts () {
     download_font "symbol.ttf"
     download_font "wingding.ttf"
     info "Updating font cache, please wait ..."
-    if [ $System == "Darwin" ];then
+    if [ $System == "Darwin" ]; then
         if [ ! -e "$HOME/Library/Fonts" ]; then
             mkdir "$HOME/Library/Fonts"
         fi
         cp $HOME/.local/share/fonts/* $HOME/Library/Fonts/
     else
-        fc-cache -fv > /dev/null
-        mkfontdir "$HOME/.local/share/fonts" > /dev/null
-        mkfontscale "$HOME/.local/share/fonts" > /dev/null
+        fc-cache -fv >/dev/null
+        mkfontdir "$HOME/.local/share/fonts" >/dev/null
+        mkfontscale "$HOME/.local/share/fonts" >/dev/null
     fi
     success "font cache done!"
 }
 # }}}
 
 ### main {{{
-main () {
+main() {
     if [ $# -gt 0 ]; then
         case $1 in
-            --uninstall|-u)
-                info "Trying to uninstall ShangVim"
-                uninstall_vim
-                echo_with_color ${BWhite} "Thanks!"
-                exit 0
-                ;;
-            --checkRequirements|-c)
-                check_requirements
-                exit 0
-                ;;
-            --install|-i)
-                welcome
-                need_cmd 'git'
-                fetch_repo
-                install_vim
-                install_package
-                install_done
-                exit 0
-                ;;
-            --help|-h)
-                usage
-                exit 0
-                ;;
-            --version|-v)
-                msg "${Version}"
-                exit 0
+        --uninstall | -u)
+            info "Trying to uninstall ShangVim"
+            uninstall_vim
+            echo_with_color ${BWhite} "Thanks!"
+            exit 0
+            ;;
+        --checkRequirements | -c)
+            check_requirements
+            exit 0
+            ;;
+        --install | -i)
+            welcome
+            need_cmd 'git'
+            fetch_repo
+            install_vim
+            install_package
+            install_done
+            exit 0
+            ;;
+        --help | -h)
+            usage
+            exit 0
+            ;;
+        --version | -v)
+            msg "${Version}"
+            exit 0
+            ;;
         esac
     else
         welcome
